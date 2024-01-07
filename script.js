974e451348c162a990e2a96d73d8d9d7993404f8
Clement Chau <142649232+Realify2005@users.noreply.github.com>
2024-01-07 20:33:23 +1100

    fix outline not showing when error

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const form = document.querySelector(".form");
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const pw = document.getElementById("password").value;
-    const confirm_pw = document.getElementById("confirm-password").value;
+    const pw = document.getElementById("password");
+    const confirm_pw = document.getElementById("confirm-password");
 
     const error = document.getElementById("pw-error");
 
-    if (pw != confirm_pw) {
+    if (pw.value != confirm_pw.value) {
         error.style.display = "block";
         pw.style.outline = "1px solid red";
         confirm_pw.style.outline = "1px solid red";
